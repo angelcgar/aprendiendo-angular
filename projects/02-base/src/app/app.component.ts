fe4e8a51223482d23e836cb73fa20cc779cfae28
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-05-15 14:00:54 -0600

    finalizar la seccion 4

--- projects/02-base/src/app/app.component.ts
+++ projects/02-base/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'base';
+  public title: string = 'Hola mundo';
+  public counter: number = 2 + 2
+  
+  
+  increaseBy(value: number):void {
+    // console.log(value.toFixed);
+    console.log('hola mundo');
+    this.counter += value
+  }
+  
+  resetValue(value: number): void {
+    this.counter = value
+  }
+
 }
+// console.log(counter);
+// console.log('hola mundo');
