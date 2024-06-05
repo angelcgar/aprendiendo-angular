98b927962c9933c501c8cbc40b9bdecd8aabb1b2
Angel Contreras Garcia <146035436+angelcgar@users.noreply.github.com>
2024-06-05 12:08:04 -0600

    finalizar la seccion 12

---
+++ projects/05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Hero, Color } from '../../interfaces/hero.interface';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styles: ``,
+})
+export class OrderComponent {
+  public isUpperCase: boolean = false;
+  public orderBy: keyof Hero | undefined | '' = '';
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue,
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.blue,
+    },
+    {
+      name: 'Daredevil',
+      canFly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.green,
+    },
+    {
+      name: 'Linterna Verde',
+      canFly: true,
+      color: Color.green,
+    },
+  ];
+
+  toogleUpperCase(): void {
+    this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: keyof Hero) {
+    this.orderBy = value;
+  }
+}
